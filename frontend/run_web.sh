50809bcca57684361abf3c35509e3591e3fbfd86
Edgar Leonardo Perez <edgarleonardo@gmail.com>
2025-11-21 23:37:03 -0400

    Ultimos cambios

--- frontend/run_web.sh
+++ frontend/run_web.sh
@@ -4,7 +4,7 @@ do
     key=$(echo $i | cut -d '=' -f 1)
     value=$(echo $i | cut -d '=' -f 2-)
     echo $key=$value
- 
+
     find /app -type f -name '*.js' -exec sed -i "s|${key}|${value}|g" '{}' +
 done
 
